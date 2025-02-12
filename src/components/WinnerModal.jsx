fb852ea5ea6f40e787a52c61167f9389ba6271a9
你的名字 <>
2025-02-12 18:21:46 +0800

    fix：解决头像显示bug

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -13,7 +13,6 @@ function WinnerModal({ winner, onClose }) {
         </button>
         <div className="modal-info-container">
           <h2 className="modal-name">{winner.name}</h2>
-          <p className="modal-info">{winner.department}</p>
           <p className="modal-description">{winner.description}</p>
         </div>
       </div>

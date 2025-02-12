9440713cdb6f87b1e0076dbbe1221662e9f49374
你的名字 <>
2025-02-12 17:41:34 +0800

    修改网站字体

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,14 +1,9 @@
 import '../styles/WinnerModal.css';
 
 function WinnerModal({ winner, onClose }) {
-  // 阻止点击模态框内容时关闭
-  const handleContentClick = (e) => {
-    e.stopPropagation();
-  };
-
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={handleContentClick}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
         <button 
           className="modal-close" 
           onClick={onClose}
@@ -16,17 +11,10 @@ function WinnerModal({ winner, onClose }) {
         >
           ✕
         </button>
-        <div className="modal-avatar">
-          <img src={winner.avatar} alt={winner.name} />
-        </div>
         <div className="modal-info-container">
           <h2 className="modal-name">{winner.name}</h2>
           <p className="modal-info">{winner.department}</p>
-          <p className="modal-info">{winner.title}</p>
-          <p className="modal-award">{winner.awardName}</p>
-          {winner.description && (
-            <p className="modal-info">{winner.description}</p>
-          )}
+          <p className="modal-description">{winner.description}</p>
         </div>
       </div>
     </div>
